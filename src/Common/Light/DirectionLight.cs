fad0ed3ce561ba27db6000c96bdb8848c9deea06
prokuror <a.sharp.fus@gmail.com>
2023-04-05 21:19:26 +0300

    *

---
+++ src/Common/Light/DirectionLight.cs
@@ -0,0 +1,18 @@
+﻿using Common.Structures;
+
+namespace Common.Light;
+
+public class DirectionLight : ILight
+{
+    public Vector Direction;
+
+    public DirectionLight(Vector direction)
+    {
+        Direction = direction.Normalize();
+    }
+
+    public float ComputeColor(Vector normal)
+    {
+        return Vector.DotProduct(Direction, normal);
+    }
+}
---
+++ src/Core/SceneObjects/Scene.cs
@@ -0,0 +1,22 @@
+﻿using Common.Light;
+using Common.Primitives;
+
+namespace Core.SceneObjects;
+
+public class Scene
+{
+    public List<ITraceable> Traceables;
+    public List<ILight> Lights;
+
+    public Scene(List<ITraceable> traceables, List<ILight> lights)
+    {
+        Traceables = traceables;
+        Lights = lights;
+    }
+
+    public Scene()
+    {
+        Traceables = new List<ITraceable>();
+        Lights = new List<ILight>();
+    }
+}
