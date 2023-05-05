f9c96a8deb1cad1aa91fd4cc9cf31b0b722e8440
lia-diez <a.sharp.fus@gmail.com>
2023-05-05 18:30:46 +0300

    matrix and multiplication added

---
+++ src/Common/Structures/Matrix.cs
@@ -0,0 +1,55 @@
+﻿namespace Common.Structures;
+
+public class Matrix
+{
+    private float[,] _rawMatrix;
+    public Vector2Int Size;
+
+    public Matrix(float[,] rawMatrix)
+    {
+        _rawMatrix = rawMatrix;
+        Size = new Vector2Int(rawMatrix.GetLength(0), rawMatrix.GetLength(1));
+    }
+    
+    public Matrix(Vector2Int size)
+    {
+        _rawMatrix = new float[size.X, size.Y];
+        Size = size;
+    }
+    
+    public Matrix(int x, int y)
+    {
+        _rawMatrix = new float[x, y];
+        Size = new Vector2Int(x, y);
+    }
+
+    public float this[int x, int y]
+    {
+        get => _rawMatrix[x, y];
+        set => _rawMatrix[x, y] = value;
+    }
+
+    public static Matrix? operator *(Matrix first, Matrix second)
+    {
+        if (first.Size.Y != second.Size.X)
+            return null;
+
+        var result = new Matrix(second.Size.Y, first.Size.X);
+
+        for (int i = 0; i < first.Size.X; i++)
+        {
+            for (int j = 0; j < second.Size.Y; j++)
+            {
+                var current = 0f;
+                for (int k = 0; k < second.Size.X; k++)
+                {
+                    current += first[i, k] * second[k, j];
+                }
+
+                result[j, i] = current;
+            }
+        }
+
+        return result;
+    }
+}
--- src/RaytracerConsole/Program.cs
+++ src/RaytracerConsole/Program.cs
@@ -1,38 +1,27 @@
-using Common.Light;
 using Common.Structures;
-using Common.Structures.Traceable;
-using Core.SceneObjects;
-using raytracer;
 
-var scene = new Scene();
-scene.Lights.Add(new DirectionLight(new Vector(1, 0, 0)));
-// scene.Traceables.Add(new Sphere(new Point(0, -2, 5), 0.5f));
-// scene.Traceables.Add(new Sphere(new Point(1, -1, 5), 1));
+float[,] f = {
+    { 1, 2, 3 },
+    { 0, 4, 0 }
+};
 
-var radius = 0.5f;
-scene.Traceables.Add(new Sphere(new Point(0, 0, 10), radius));
-for (int i = 1; i < 5; i++)
+float[,] s =
 {
-    scene.Traceables.Add(new Sphere(new Point(-i, i, 10), radius));
-    scene.Traceables.Add(new Sphere(new Point(i, i, 10), radius));
-    scene.Traceables.Add(new Sphere(new Point(i, -i, 10), radius));
-    scene.Traceables.Add(new Sphere(new Point(-i, -i, 10), radius));
-    scene.Traceables.Add(new Sphere(new Point(0, i, 10), radius));
-    scene.Traceables.Add(new Sphere(new Point(0, -i, 10), radius));
-    scene.Traceables.Add(new Sphere(new Point(i, 0, 10), radius));
-    scene.Traceables.Add(new Sphere(new Point(-i, 0, 10), radius));
+    { 0, 1, 3, 2 },
+    { 2, 4, 0, 1 },
+    { 4, 3, 1, 2 }
+};
+
+Matrix first = new Matrix(f);
+Matrix second = new Matrix(s);
+
+var a = first * second;
+for (int i = 0; i < a.Size.X; i++)
+{
+    for (int j = 0; j < a.Size.Y; j++)
+    {
+        Console.Write(a[i,j] + " ");
+    }
+
+    Console.WriteLine();
 }
-// scene.Traceables.Add(new Sphere(new Point(-2, -3, 5), 0.5f));
-
-var camera = new Camera(new CameraSettings()
-{
-    Fov = 30,
-    Resolution =  new Vector2Int(1024,1024)
-}, scene);
-var bitmap = camera.Render();
-var stream = File.Open("pic.bmp", FileMode.OpenOrCreate);
-var exporter = new BmpImageExporter(stream, bitmap);
-exporter.Export();
-stream.Close();
-
-
